768be88401d30c8cd945f497e789ca126e0c2c39
hadh93 <hoondog1993@gmail.com>
2023-01-26 13:43:30 -0600

    minor modification

--- Ch3_Functions/00_define_a_function.dart
+++ Ch3_Functions/00_define_a_function.dart
@@ -1,7 +1,13 @@
 String sayHello(String name){
   return "Hello $name nice to meet you!";
+  // fat arrow 를 사용할 수도 있다.
+  // e.g.
+  // => return "Hello World!"
 }
+
+num plus (num a, num b) => a + b;
 
 void main(){
   print(sayHello("thomas"));
+  print(plus(10,5));
 }
